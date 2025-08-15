275d10f901f4581614a065104c41e1b72221cf43
Kostya Gorodnichuk <gorodnichuk@trafory.com>
2025-08-15 22:25:28 +0300

    add root route

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ app.use((req, res, next) => {
   next()
 })
 
+app.get('/', (req, res) => {
+  res.send('Working')
+})
+
 app.get('/generate', async (req, res) => {
   const { link, color1, color2, width, margin, pattern } = req.query
 
